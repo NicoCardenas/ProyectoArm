1d63fd8936c8e9340f1a36be33253e0873e9688e
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2018-05-15 20:22:45 -0500

    Principal update
    Co-Authored-By: PaoCuellar <paocuellar@users.noreply.github.com>

--- Proyecto.s
+++ Proyecto.s
@@ -1,11 +1,11 @@
 .text
 .global _start
-_start:	MOV R0, #0x11 @ x1
-		MOV R1, #0x12 @ y1
-		MOV R2, #0x13 @ x2
-		MOV R3, #0x14 @ y2
-		MOV R4, #0x15 @ x3
-		MOV R5, #0x16 @ y3
+_start:	MOV R0, #2 @ x1
+		MOV R1, #6 @ y1
+		MOV R2, #9 @ x2
+		MOV R3, #12 @ y2
+		MOV R4, #15 @ x3
+		MOV R5, #20 @ y3
 
 		SUB R6, R2, R0 @ x2 - x1
 		SUB R7, R3, R1 @ y2 - y1
@@ -17,7 +17,7 @@ _start:	MOV R0, #0x11 @ x1
 		SUB R8, R3, R5 @ y2 - y3
 		MUL R9, R7, R7 @ (x2 - x3)**2
 		MUL R10, R8, R8 @ (y2 - y3)**2
-		ADD R7, R8, R10 @ (x2 - x3)**2 + (y2 - y3)**2
+		ADD R7, R9, R10 @ (x2 - x3)**2 + (y2 - y3)**2
 
 		SUB R8, R0, R4 @ x1 - x3
 		SUB R9, R1, R5 @ y1 - x3
@@ -25,35 +25,100 @@ _start:	MOV R0, #0x11 @ x1
 		MUL R11, R9, R9 @ (y1 - y3)**2
 		ADD R8, R10, R11 @ (x1 - x3)**2 + (y1 - y3)**2
 
-		CMP R6, #0x0 @ R6 == 0
-		BEQ	CANCEL @ if R6 == 0 then jump FIN
+		MOV R0, R6 @ lado a = R0
+		MOV R1, R7 @ lado b = R1
+		MOV R2, R8 @ lado c = R2
 
-		CMP R7, #0x0 @ R7 == 0
-		BEQ	CANCEL @ if R7 == 0 then jump FIN
+		MOV R7, R0 @ a = raiz(lado a)
+		MOV R8, #0 @ b = 0
+		MOV R9, #0 @ c = 0
+		BL RAIZ
+		SUB R8, R8, #1 @ raiz((x2 - x1)**2 + (y2 - y1)**2)
+		MOV R0, R8
+
+		MOV R7, R1 @ a = raiz(lado b)
+		MOV R8, #0 @ b = 0
+		MOV R9, #0 @ c = 0
+		BL RAIZ
+		SUB R8, R8, #1 @ raiz((x2 - x3)**2 + (y2 - y3)**2)
+		MOV R1, R8
+
+		MOV R7, R2 @ a = raiz(lado c)
+		MOV R8, #0 @ b = 0
+		MOV R9, #0 @ c = 0
+		BL RAIZ
+		SUB R8, R8, #1 @ raiz((x1 - x3)**2 + (y1 - y3)**2)
+		MOV R2, R8
+
+		CMP R0, #0x0 @ R0 == 0
+		BEQ	CANCEL @ if R0 == 0 then jump FIN
+
+		CMP R1, #0x0 @ R1 == 0
+		BEQ	CANCEL @ if R1 == 0 then jump FIN
 		
-		CMP R8, #0x0 @ R8 == 0
-		BEQ	CANCEL @ if R8 == 0 then jump FIN
+		CMP R2, #0x0 @ R2 == 0
+		BEQ	CANCEL @ if R2 == 0 then jump FIN
 
-		ADD R9, R6, R7
-		CMP R9, R8
-		BEQ CANCEL
-		ADD R10, R9, R8
+		@ Perimetro
+		ADD R3, R0, R1
+		ADD R3, R2, R3
 
-DIV:	CMP R11, R12
-		BGT FIN
-		SUB R11, R11, R12
-		ADD R13, #1, R13
+		@ Area
+		ADD R4, R1, R2 @ lado b + lado c
+		MUL R4, R3, R4 @ perimetro * (lado b + lado c)
+		ADD R5, R0, R2 @ lado a + lado c
+		MUL R4, R5, R4 @ perimetro * (lado b + lado c) * (lado a + lado c)
+		ADD R5, R0, R1 @ lado a + lado b
+		MUL R4, R5, R4 @ perimetro * (lado b + lado c) * (lado a + lado c) * (lado a + lado b)
+		@ division
+		MOV R10, R4 @ x = perimetro * (lado b + lado c) * (lado a + lado c) * (lado a + lado b)
+		MOV R11, #16 @ y = 2
+		MOV R12, #0 @ count = 0
+		BL DIV
+		MOV R4, R12
+		@ raiz
+		MOV R7, R4 @ a = raiz(lado c)
+		MOV R8, #0 @ b = 0
+		MOV R9, #0 @ c = 0
+		BL RAIZ
+		SUB R8, R8, #1 @ raiz((x1 - x3)**2 + (y1 - y3)**2)
+		MOV R4, R8
+
+		CMP R4, #0x0 @ R4 == 0
+		BEQ	CANCEL @ if R4 == 0 then jump FIN
+		B 	FIN
+
+RAIZ:	MOV R10, R7 @ x = a
+		MOV R11, #2 @ y = 2
+		MOV R12, #0 @ count = 0
+		MOV SP, LR
+		BL 	DIV @ x div y
+		MOV R8, R12 @ b = a div 2
+		MUL	R9, R8, R8 @ c = b ^ 2
+RLOOP:	CMP R9, R7 @ c > a
+		BLT RLOOP2
+		MOV R10, R8 @ x = b
+		MOV R12, #0 @ count = 0
+		BL 	DIV @ x div 2
+		MOV R8, R12 @ b = b div 2
+		MUL	R9, R8, R8 @ c = b ^ 2
+		B 	RLOOP
+RLOOP2:	CMP R9, R7 @ c <= a
+		MOV LR, SP
+		BXGT LR
+		ADD R8, R8, #1 @ b += 1
+		MUL R9, R8, R8 @ c = b ^ 2
+		B RLOOP2
+
+DIV:	CMP R10, R11 @ x < y
+		BXLT LR
+		SUB R10, R10, R11 @ x -= y
+		ADD R12, R12, #1 @ count += 1
 		B 	DIV
-
-RAIZ:	
-
-		@ skipcond
-		@CMP R0, R2
-		@BLT FIN
 
 CANCEL: MOV R0, #-1
 		B FIN
-		MOV R0, R6 @ echo $? respuesta
+		@ MOV R0, R6 @ echo $? respuesta
 FIN:	MOV R7, #1
 		SVC 0
 .end
--- Raiz.s
+++ Raiz.s
@@ -1,37 +1,41 @@
 .text
 .global _start
-_start:	MOV R0, #0 @ a = R0
+_start:	MOV R0, #150 @ a = R0
 		MOV R1, #0 @ b = R1
 		MOV R2, #0 @ c = R2
 
-		MOV R11, #0 @ x = R11
-		MOV R12, #0 @ y = R12
-		MOV R13, #0 @ count = R13
+		MOV R10, #0 @ x = R10
+		MOV R11, #0 @ y = R11
+		MOV R12, #0 @ count = R12
 
-RAIZ:	MOV R11, R0 @ x = a
-		MOV R12, #2 @ y = 2
-		B 	DIV @ x div y
-		MOV R1, R13 @ b = a div 2
+RAIZ:	MOV R10, R0 @ x = a
+		MOV R11, #2 @ y = 2
+		MOV R12, #0 @ count = 0
+		BL 	DIV @ x div y
+		MOV R1, R12 @ b = a div 2
 		MUL	R2, R1, R1 @ c = b ^ 2
-RLOOP:	CMP R2, R0 @ c < a
+RLOOP:	CMP R2, R0 @ c > a
 		BLT RLOOP2
-		MOV R11, R1 @ x = b
-		B 	DIV @ x div 2
-		MOV R1, R13 @ b = b div 2
+		MOV R10, R1 @ x = b
+		MOV R12, #0 @ count = 0
+		BL 	DIV @ x div 2
+		MOV R1, R12 @ b = b div 2
 		MUL	R2, R1, R1 @ c = b ^ 2
 		B 	RLOOP
-RLOOP2:	CMP R2, R0 @ c != a
-		BNE	FIN
+RLOOP2:	CMP R2, R0 @ c <= a
+		BGT	FIN
 		ADD R1, R1, #1 @ b += 1
 		MUL R2, R1, R1 @ c = b ^ 2
 		B RLOOP2
 
-DIV:	CMP R11, R12 @ x < y
-		BLLT PC 
-		SUB R11, R11, R12 @ x -= y
-		ADD R13, R13, #1 @ count += 1
+DIV:	CMP R10, R11 @ x < y
+		BXLT LR
+		SUB R10, R10, R11 @ x -= y
+		ADD R12, R12, #1 @ count += 1
 		B 	DIV
 
-FIN:	MOV R7, #1
+FIN:	SUB R1, R1, #1
+		MOV R0, R1
+		MOV R7, #1
 		SVC 0
 .end
