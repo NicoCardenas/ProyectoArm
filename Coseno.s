4ac9764f8aa44462bd2563faa7edfa1a63f54447
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2018-05-21 15:26:48 -0500

    correcion del coseno

--- Coseno.s
+++ Coseno.s
@@ -1,8 +1,8 @@
 .text
 .global _start
-_start:	LDR R0, =#28300 @ x = R0
-		LDR R1, =#40000 @ y = R1
-		LDR R2, =#28300 @ count = R2
+_start:	LDR R0, =#53900 @ x = R0
+		LDR R1, =#60000 @ y = R1
+		LDR R2, =#60000 @ count = R2
 
 		@ div( (a*a)+(b*b)-(c*c), 2*a*b) = cosc
 		MUL R6, R0, R0
@@ -16,25 +16,12 @@ _start:	LDR R0, =#28300 @ x = R0
 		MOV R7, R8
 		@ Division
 		CMP R6, #0
-		MOVEQ R6, #0
-		BGT APOS
-		BLT ANG
-
-APOS:	CMP R6, R7
-		MOVLT R6, #0
-		MOVGT R6, #1
-		B COSA
-
-ANG:	MOV R12, R6
-		BL 	ABS
-		MOV R6, R12
-		CMP R6, R7
-		MOVLT R6, #0
 		MOVGT R6, #-1
-		B COSA
+		MOVEQ R6, #0
+		MOVLT R6, #1
 
 		@ div( (a*a)+(c*c)-(b*b), 2*a*c) = cosb
-COSA:	MUL R7, R0, R0
+		MUL R7, R0, R0
 		MUL R8, R1, R1
 		MUL R9, R2, R2
 		ADD R7, R9, R7
@@ -43,24 +30,11 @@ COSA:	MUL R7, R0, R0
 		MUL R9, R8, R2
 		@ Division
 		CMP R7, #0
-		MOVEQ R7, #0
-		BGT BPOS
-		BLT BNG
-
-BPOS:	CMP R7, R8
-		MOVLT R7, #0
-		MOVGT R7, #1
-		B COSC
-
-BNG:	MOV R12, R7
-		BL 	ABS
-		MOV R7, R12
-		CMP R7, R8
-		MOVLT R7, #0
 		MOVGT R7, #-1
-		B COSC
+		MOVEQ R7, #0
+		MOVLT R7, #1
 
-COSC:	@ div( (b*b)+(c*c)-(a*a), 2*c*b) = cosa
+		@ div( (b*b)+(c*c)-(a*a), 2*c*b) = cosa
 		MUL R8, R0, R0
 		MUL R9, R1, R1
 		MUL R10, R2, R2
@@ -70,24 +44,11 @@ COSC:	@ div( (b*b)+(c*c)-(a*a), 2*c*b) = cosa
 		MUL R8, R2, R8
 		@ Division
 		CMP R8, #0
-		MOVEQ R8, #0
-		BGT CPOS
-		BLT CNG
-
-CPOS:	CMP R8, R9
-		MOVLT R8, #0
-		MOVGT R8, #1
-		B COMP
-
-CNG:	MOV R12, R8
-		BL 	ABS
-		MOV R8, R12
-		CMP R8, R9
-		MOVLT R8, #0
 		MOVGT R8, #-1
-		B COMP
+		MOVEQ R8, #0
+		MOVLT R8, #1
 
-COMP:	@ if (cosa == 0| cosb == 0| cosc == 0)
+		@ if (cosa == 0| cosb == 0| cosc == 0)
 		CMP R6, #0
 		BEQ RECT
 		CMP R7, #0
